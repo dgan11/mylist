fc312afcd76f382c8d06d8c909a81cc62cc03b3d
David Gan <dgan979@gmail.com>
2022-06-26 13:15:17 -0700

    components

---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <h1>My List</h1>
+            </div>
+            <a>Home</a>
+            <a>About</a>
+            <a>Campaigns</a>
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const about = () => {
+  return (
+    <div>
+        <h1>Hello</h1>
+        <p>Nice to meet you</p>
+    </div>
+  )
+}
+
+export default about;
